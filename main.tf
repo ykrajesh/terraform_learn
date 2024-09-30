9ccb4e9f749c15da2e03111ecb010b875675fc00
YADAV <rajesh.c.yadav@capgemini.com>
2024-10-01 03:49:28 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  jlisjdfkljskd
+  features {
+    
+  }
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
 }
